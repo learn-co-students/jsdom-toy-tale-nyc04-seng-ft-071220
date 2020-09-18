445f5db3560641a92cecb4951d44e44c979193c4
Chenyun Zhang <morningchenyun@gmail.com>
2020-09-18 10:48:49 -0400

    working on the first deliverable

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
 let addToy = false;
+let url = "http://localhost:3000/toys"
+let toyCollection = document.querySelector("#toy-collection");
+
 
 document.addEventListener("DOMContentLoaded", () => {
   const addBtn = document.querySelector("#new-toy-btn");
@@ -12,4 +15,30 @@ document.addEventListener("DOMContentLoaded", () => {
       toyFormContainer.style.display = "none";
     }
   });
+
+  
+  fetch(url)
+  .then(res => res.json())
+  .then((toys) => {
+    toys.forEach(toy=>{
+      // create div tag, add class to it
+      let cardObj = document.createElement("div");
+      cardObj.className = "card";
+      let toyName = document.createElement("h2");
+      toyName.innerText = toy.name;
+      let toyImg = document.createElement("img");
+      toyImg.src = toy.image
+      toyImg.className = "toy-avatar"
+      let toyLikes = document.createElement("p");
+      // toyLikes.inner = `${counter} Likes`
+      let toyButton = document.createElement("button");
+      toyButton.className = "like-btn"
+
+      // append the parent
+      toyCollection.append(cardObj);
+    })
+  })
+
+
+
 });

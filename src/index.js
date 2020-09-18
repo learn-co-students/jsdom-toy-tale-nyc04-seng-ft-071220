d4052a9543afb9882d1dbd92717e83b8279ef59b
Elronia <zar.catherine@gmail.com>
2020-09-18 10:59:29 -0400

    defining addToyInfo function

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 let addToy = false;
 
+function addToyInfo 
 document.addEventListener("DOMContentLoaded", () => {
   const addBtn = document.querySelector("#new-toy-btn");
   const toyFormContainer = document.querySelector(".container");
+  const toyCollection = document.querySelector("#toy-collection");
   addBtn.addEventListener("click", () => {
     // hide & seek with the form
     addToy = !addToy;
